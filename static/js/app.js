73cec7ccba2b7a6579067be69678035a36b49558
Greg Presneill <45741579+enumerbs@users.noreply.github.com>
2024-08-22 23:03:20 +1000

    feat: implement display of an individual sample's metadata (demographic information)

--- static/js/app.js
+++ static/js/app.js
@@ -3,20 +3,26 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    const metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let sampleMetadata = metadata.filter(item => item.id == sample.id)[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let sampleMetadataPanel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    sampleMetadataPanel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    let sampleMetadataKeys = Object.keys(sampleMetadata);
+    let sampleMetadataValues = Object.values(sampleMetadata);
+    let sampleMetadataHtmlString = ""
+    for (i=0; i < sampleMetadataKeys.length; i++) {
+      sampleMetadataHtmlString += `<p><strong>${sampleMetadataKeys[i].toUpperCase()}</strong>: ${sampleMetadataValues[i]}</p>`;
+    }
+    sampleMetadataPanel.html(sampleMetadataHtmlString);
   });
 }
 
